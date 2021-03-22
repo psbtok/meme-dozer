d7165dd09ffb27ff814af85473facb8eff508502
petosbratok <44753312+petosbratok@users.noreply.github.com>
2021-03-22 21:55:07 +0300

    commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = "..\django_dozer\\staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ["..\django_dozer\\blog\\static"]
+STATICFILES_DIRS = ['..\\django_dozer\\blog\\static',]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
