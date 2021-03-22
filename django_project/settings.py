4b7586e5dd7585819b35b561c52c621b0a700f57
petosbratok <44753312+petosbratok@users.noreply.github.com>
2021-03-22 21:32:02 +0300

    preparation for heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = "..\django_dozer\\staticfiles"
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ["..\django_dozer\\blog\\static"]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
