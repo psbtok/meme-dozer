3b598758605be6847fdeadd96a4792f8f26d6a05
petosbratok <44753312+petosbratok@users.noreply.github.com>
2021-03-22 15:40:32 +0300

    tiny style adjustments

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.db.models import Count
 from django.views.generic import (
     ListView,
     DetailView,
